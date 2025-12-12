5f9bd91898c37626bfb15dc0af8d791186a68d70
Gaurish Gangwar <g.gangwar@qibitech.com>
2025-12-12 19:07:29 +0900

    Publish lidar and odometry data with reliable QoS, as expected by AMCL and other Nav2 nodes

--- ros2/kachaka_grpc_ros2_bridge/src/component/lidar_component.cpp
+++ ros2/kachaka_grpc_ros2_bridge/src/component/lidar_component.cpp
@@ -32,7 +32,8 @@ class LidarComponent : public rclcpp::Node {
     frame_prefix_ = this->get_parameter("frame_prefix").as_string();
     stub_ = GetSharedStub(declare_parameter("server_uri", ""));
 
-    rclcpp::SensorDataQoS qos;
+    // rclcpp::SensorDataQoS qos;
+    rclcpp::SystemDefaultsQoS qos;
     using namespace std::placeholders;
     scan_bridge_ =
         std::make_unique<Ros2TopicBridge<kachaka_api::GetRosLaserScanResponse,
